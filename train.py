b04a71318d2f3b39d10fee853015603946de8a12
tanweizhen <wtan009@gmail.com>
2024-02-25 22:06:24 +0800

    Training code update

--- train.py
+++ train.py
@@ -2,12 +2,13 @@ from ultralytics import YOLO
 from args_ import *
 import argparse
 
-# Train YOLOv5s on dataset for 100 epochs
+
 def train(args):
-    #model = YOLO("yolov8n.pt")
-    #model.train(data=args.dir + "/dataset/Apex_total/data.yaml", epochs=300, batch=20, imgsz=(416,416), workers=16, val=False)
-    model = YOLO(args.dir+"/model/apex_total_8n_350_1000.pt")
-    model.val(data=args.dir + "/dataset/Apex_total/data.yaml")
+    data_path = "/dataset/firstset/data.yaml"
+    model = YOLO("Model/apex_8s.pt")
+    model.train(data=args.dir + data_path, epochs=10, batch=20, workers=8, val=False)
+    #model = YOLO(args.dir+"/model/apex_total_8n_350_1000.pt")
+    model.val(data=args.dir + data_path)
 
 
 if __name__ == "__main__":
