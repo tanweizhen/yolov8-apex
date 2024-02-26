96f6d0ec0e8fd81160777bfc8ac97f917c026059
tanweizhen <wtan009@gmail.com>
2024-02-26 08:51:21 +0800

    I have no idea what a relative path is apparently

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import argparse
 
 def train(args):
     data_path = "/dataset/firstset/data.yaml"
-    model = YOLO("/runs/detect/train/weights/best.pt")
+    model = YOLO("runs/detect/train/weights/best.pt")
     model.train(data=args.dir + data_path, epochs=100, batch=20, workers=8, val=True)
     #model = YOLO(args.dir+"/model/apex_total_8n_350_1000.pt")
     #model.val(data=args.dir + data_path)
