4b9b452479ab24492692b0405b6ed3b42c9cda87
tanweizhen <wtan009@gmail.com>
2024-02-26 08:50:31 +0800

    Typo path

--- train.py
+++ train.py
@@ -5,11 +5,10 @@ import argparse
 
 def train(args):
     data_path = "/dataset/firstset/data.yaml"
-    model = YOLO("runs/detect/train/best.pt")
+    model = YOLO("/runs/detect/train/weights/best.pt")
     model.train(data=args.dir + data_path, epochs=100, batch=20, workers=8, val=True)
     #model = YOLO(args.dir+"/model/apex_total_8n_350_1000.pt")
-    model.val(data=args.dir + data_path)
-
+    #model.val(data=args.dir + data_path)
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
